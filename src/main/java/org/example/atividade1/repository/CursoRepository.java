523f4a82c3effa7092951f27a0cfb00cc817bcf4
Lucas Leite Lessa <54894793+LucasLeiteLessa@users.noreply.github.com>
2026-03-05 18:17:46 -0300

    feat: adicionar repositórios JPA para Aluno e Curso

---
+++ src/main/java/org/example/atividade1/repository/CursoRepository.java
@@ -0,0 +1,11 @@
+package org.example.atividade1.repository;
+
+import org.example.atividade1.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+}
+
